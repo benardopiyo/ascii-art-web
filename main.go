7bee31920b092a99de1c838c9ece765589babae0
beopiyo <benardopiyo13@gmail.com>
2024-06-12 15:44:58 +0300

    feat: add main.go file

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	server "ascii-art-web/server"
+)
+
+func main() {
+	server.InitTemplates()
+	server.SetupRoutes()
+	log.Println("Server is running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
